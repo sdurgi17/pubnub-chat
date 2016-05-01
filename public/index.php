23815faa98d18d16c5c7e93f2794b76d301ae35c
srikar-bhargav-durgi <sdurgi17@gmail.com>
2016-05-01 23:35:12 +0530

    simplest working groupchat concept. subscribe on frontend. publish on backend.

--- public/index.php
+++ public/index.php
@@ -10,19 +10,22 @@ Router::route('/^\/index$/', function() {
 	require 'index.html';
 });
 
+Router::route('/^\/publish$/', function(){
+	$message = $_REQUEST['message'];
+	$pubnub = new Pubnub('pub-c-ad97200f-fadb-4ac7-b41e-382166985533', 'sub-c-359e78d4-0ddf-11e6-a6c8-0619f8945a4f'); 
+	$publish_result = $pubnub->publish('group_chat',$message);
+});
+
 Router::route('/^\/show_data$/', function() {
 	echo "there there";
 
 	$pubnub = new Pubnub('demo', 'demo');
  
 	//Subscribe to a channel, this is not async.
-	$pubnub->subscribe('hello_world', function ($envelope) {
-             print_r($envelope['message']);
-       	});
  
 	// Use the publish command separately from the Subscribe code shown above. 
 	// Subscribe is not async and will block the execution until complete.
-	$publish_result = $pubnub->publish('hello_world','Hello PubNub!');
+	$publish_result = $pubnub->publish('group_message','Hello PubNub!');
 	print_r($publish_result);
 
 
